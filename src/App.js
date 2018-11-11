9bb906a49dd6d878a07a7060c64905253b2fefe3
Oswaldo <oswaldo.vazquez@gmail.com>
2018-11-11 16:38:14 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Header from './componentes/Header';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header titulo= 'Noticias' />
+      </div>
+    );
+  }
+}
+
+export default App;
