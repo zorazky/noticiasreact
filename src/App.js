9734cf06cf50cffd36e1f2e460573e3d0a1f5e8c
Oswaldo <oswaldo.vazquez@gmail.com>
2018-11-11 18:25:54 -0600

    form is working to change the category

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from './componentes/Header';
 import Noticias from './componentes/Noticias';
+import Formulario from './componentes/Formulario';
 
 class App extends Component {
   state = {
@@ -10,8 +11,10 @@ class App extends Component {
     this.consultarNoticias();
   }
 
-  consultarNoticias = () => {
-    let url = 'https://newsapi.org/v2/top-headlines?country=mx&category=general&apiKey=d539c47efc784013a19afeb5a34ab049';
+  consultarNoticias = (categoria = 'general') => {
+    //console.log(categoria)
+    let url = `https://newsapi.org/v2/top-headlines?country=mx&category=${categoria}&apiKey=d539c47efc784013a19afeb5a34ab049`;
+    console.log(url);
     fetch(url)
       .then(respuesta => {
         return respuesta.json();
@@ -29,6 +32,7 @@ class App extends Component {
       <div className="contenedor-app">
         <Header titulo='Noticias' />
         <div className="container white contenedor-noticias">
+          <Formulario consultarNoticias={this.consultarNoticias} />
           <Noticias noticias={this.state.noticias} />
         </div>
       </div>
---
+++ src/componentes/Formulario.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+class Formulario extends Component {
+
+    categoriaRef = React.createRef();
+
+    cambiarCategoria = (e) => {
+        e.preventDefault();
+
+        this.props.consultarNoticias(this.categoriaRef.current.value);
+        //console.log(this.categoriaRef.current.value);
+    }
+    render() {
+        return (
+            <div className="buscador row">
+                <div className="col s12 m8 offset-m2">
+                    <form onSubmit={this.cambiarCategoria}>
+                        <h2>Noticias por Categoria</h2>
+                        <div className="input-field col s12 m8">
+                            <select ref={this.categoriaRef}>
+                                <option value="general" defaultValue>General</option>
+                                <option value="business">Negocios</option>
+                                <option value="entertaiment">Entretenimiento</option>
+                                <option value="health">Salud</option>
+                                <option value="science">Ciencia</option>
+                                <option value="sports">Deportes</option>
+                                <option value="technology">Tecnología</option>
+                            </select>
+                        </div>
+                        <div className="input-field col s12 m4 enviar">
+                            <input type="submit" className="btn amber darken-2" value="Buscar" />
+                        </div>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Formulario;
